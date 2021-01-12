4abfc3518bdb635fcbd69dfabaed021218a4794b
Tony Chou <tcheon8788@gmail.com>
2021-01-13 04:09:55 +0800

    update

---
+++ Pracitces/48.rotate-image.cpp
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=48 lang=cpp
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+
+        for(int i = 0; i < n; i ++) 
+            for(int j = 0; j < i; j ++) 
+                swap(matrix[i][j], matrix[j][i]);
+            
+        for(int i = 0; i < n; i ++) {
+            reverse(matrix[i].begin(), matrix[i].end());
+        }
+    }
+};
+// @lc code=end
+
