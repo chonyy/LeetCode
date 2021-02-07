4b963e5fc0d73517006f845ea5f19d0609f330d0
Tony Chou <tcheon8788@gmail.com>
2021-02-08 04:22:01 +0800

    2021/02/08

--- 98.validate-binary-search-tree.cpp
+++ 98.validate-binary-search-tree.cpp
@@ -19,7 +19,20 @@
 class Solution {
 public:
     bool isValidBST(TreeNode* root) {
-        
+        return helper(root, nullptr, nullptr);
+    }
+
+    bool helper(TreeNode* root, TreeNode* minNode, TreeNode* maxNode) {
+        if(!root)
+            return true;
+
+        if(minNode && root->val <= minNode->val)
+            return false;
+
+        if(maxNode && root->val >= maxNode->val)
+            return false;
+
+        return helper(root->left, minNode, root) && helper(root->right, root, maxNode);
     }
 };
 // @lc code=end
