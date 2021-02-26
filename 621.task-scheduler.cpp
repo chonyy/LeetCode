f28964f5510e7a79e6abfada7459ec20b9d2c00f
Chonyy <tcheon8788@gmail.com>
2021-02-26 22:15:46 +0800

    2021/02/26

---
+++ 621.task-scheduler.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=621 lang=cpp
+ *
+ * [621] Task Scheduler
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int leastInterval(vector<char>& tasks, int n) {
+        
+    }
+};
+// @lc code=end
+
