6ad53a9f665cde23339bfbf752685274528db0a2
Chonyy <tcheon8788@gmail.com>
2021-03-11 11:35:31 +0800

    2021/03/11

---
+++ 767.reorganize-string.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=767 lang=cpp
+ *
+ * [767] Reorganize String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string reorganizeString(string S) {
+        unordered_map<char, int> count;
+
+        for(char c : S) {
+            count[c] ++;
+        }
+
+        
+    }
+};
+// @lc code=end
+
