476f469b61721255fa5be92cc5f884ba147226ee
Tony Chou <tcheon8788@gmail.com>
2021-03-25 03:26:28 +0800

    2021/03/25

--- test.cpp
+++ test.cpp
@@ -2,24 +2,30 @@
 
 using namespace std;
 
-void solve(vector<int>& nums) {
-	for(int i : nums) {
-    	cout << i << endl;
+void solve(long long num) {
+    vector<pair<long long, string>> units = {{10000000, "kuti"}, {100000, "lakh"}, {1000, "hajar"}, {100, "shata"}};
+    for(auto unit : units) {
+        if(num >= unit.first) {
+            solve(num / unit.first);
+            cout << " " << unit.second;
+            num = num % unit.first;
+        }
     }
+    if(num)
+        cout << " " << num;
 }
 
 int main() {
-    int n;
-    cin >> n;
-    vector<int> nums;
-    int temp;
-
-    while(n--) {
-        cin >> temp;
-        nums.push_back(temp);
+    long long num;
+    int k = 1;
+    while(cin >> num) {
+        cout << setw(4) << k << ".";
+        if(num)
+            solve(num);
+        else
+            cout << " 0";
+        cout << endl;
+        k ++;
     }
-
-    solve(nums);
-
     return 0; 
 }
